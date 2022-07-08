03dea34a25756b61827da7781ed1961ed9b84323
LeonardoCaro27 <leocaromiranda@gmail.com>
2022-07-08 14:06:15 -0500

    Proyecto

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+
+  slideOpt={
+    initialSlide: 0, //slide inicial
+    slidesPerView: 1, //slide por vista
+    centeredSlides: true, //que las slides esten centradas
+    speed: 400 // velocidad de transicion de cada slide en milisegundo
+  }
+  constructor() {}
+
+}
