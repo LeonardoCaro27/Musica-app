aed8f36efaf8b44fa829cc0d090f16d749b9df25
LeonardoCaro27 <leocaromiranda@gmail.com>
2022-07-09 22:40:02 -0500

    proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,8 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
 import {IonicStorageModule} from '@ionic/storage-angular';
+import { ReactiveFormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [AppComponent],
